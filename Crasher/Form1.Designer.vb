6aaeb91066aa76a47a0f0fa70f2e5367f12b3b38
Shubhang Rathore <shubhang92@gmail.com>
2014-05-27 20:05:29 +0530

    Initial commit - Crasher v1.0

---
+++ Crasher/Form1.Designer.vb
@@ -0,0 +1,259 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ContextMenuStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Showcard Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(237, 225)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(121, 37)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Crash!"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append
+        Me.ComboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
+        Me.ComboBox1.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox1.ForeColor = System.Drawing.Color.Black
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(30, 134)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(130, 24)
+        Me.ComboBox1.TabIndex = 2
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "Crasher"
+        Me.NotifyIcon1.Visible = True
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.BackColor = System.Drawing.Color.White
+        Me.ContextMenuStrip1.Font = New System.Drawing.Font("Showcard Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(108, 26)
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(107, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit!"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(210, 135)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(154, 26)
+        Me.TextBox2.TabIndex = 4
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox3.Location = New System.Drawing.Point(391, 135)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(73, 26)
+        Me.TextBox3.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Gabriola", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(50, 96)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(90, 35)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Server Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Gabriola", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(248, 96)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(79, 35)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "IP Address"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Gabriola", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(408, 96)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 35)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Port"
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Showcard Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(505, 107)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(70, 35)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "Save"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Font = New System.Drawing.Font("Showcard Gothic", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(171, 19)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(175, 44)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Crasher"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.PictureBox1.Location = New System.Drawing.Point(361, 18)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(63, 46)
+        Me.PictureBox1.TabIndex = 11
+        Me.PictureBox1.TabStop = False
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.BackColor = System.Drawing.Color.Transparent
+        Me.LinkLabel1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.LinkLabel1.Font = New System.Drawing.Font("Georgia", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LinkLabel1.LinkColor = System.Drawing.Color.White
+        Me.LinkLabel1.Location = New System.Drawing.Point(503, 248)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(79, 14)
+        Me.LinkLabel1.TabIndex = 13
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "DISCLAIMER"
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Showcard Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(505, 148)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(70, 35)
+        Me.Button3.TabIndex = 14
+        Me.Button3.Text = "Delete"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.Transparent
+        Me.Label5.Font = New System.Drawing.Font("Gabriola", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.White
+        Me.Label5.Location = New System.Drawing.Point(367, 130)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(18, 35)
+        Me.Label5.TabIndex = 15
+        Me.Label5.Text = ":"
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.Button1
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(594, 274)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Crasher"
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents NotifyIcon1 As System.Windows.Forms.NotifyIcon
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+
+End Class
---
+++ Crasher/Form1.vb
@@ -0,0 +1,236 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Public appdataPath As String = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
+
+    Public ip As String
+    Public port As String
+    Public count As System.Collections.ObjectModel.ReadOnlyCollection(Of String)
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        If TextBox2.Text = "" Then
+            NotifyIcon1.BalloonTipIcon = ToolTipIcon.Error
+            NotifyIcon1.BalloonTipTitle = "Info Missing"
+            NotifyIcon1.BalloonTipText = "Enter IP address!"
+            NotifyIcon1.ShowBalloonTip(7000)
+        ElseIf TextBox3.Text = "" Then
+
+            NotifyIcon1.BalloonTipIcon = ToolTipIcon.Error
+            NotifyIcon1.BalloonTipTitle = "Info Missing"
+            NotifyIcon1.BalloonTipText = "Enter Port!"
+            NotifyIcon1.ShowBalloonTip(7000)
+        Else
+
+            Try
+
+
+                Dim SetupPath As String = Application.StartupPath & "\crasherbase"
+
+                
+                Dim mypro As New Process
+                mypro.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+                mypro.StartInfo.FileName = SetupPath
+                mypro.StartInfo.Arguments = " " & TextBox2.Text & " " & TextBox3.Text
+                mypro.Start()
+
+
+
+                NotifyIcon1.BalloonTipIcon = ToolTipIcon.Info
+                NotifyIcon1.BalloonTipTitle = "HACKED!"
+                NotifyIcon1.BalloonTipText = "Check the Server, should have crashed!"
+                NotifyIcon1.ShowBalloonTip(7000)
+
+
+
+            Catch ex As Exception
+                'MsgBox(ex.Message)
+                NotifyIcon1.BalloonTipIcon = ToolTipIcon.Error
+                NotifyIcon1.BalloonTipTitle = "Something went Wrong"
+                NotifyIcon1.BalloonTipText = "Coudn't execute the Hack! Try again"
+                NotifyIcon1.ShowBalloonTip(7000)
+
+
+            End Try
+
+
+
+
+
+        End If
+
+
+
+
+
+       
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        NotifyIcon1.BalloonTipTitle = "Welcome"
+        NotifyIcon1.BalloonTipText = "Let's crash some Servers!"
+        NotifyIcon1.ShowBalloonTip(7000)
+
+
+        Dim folderExists As Boolean
+        folderExists = My.Computer.FileSystem.DirectoryExists(appdataPath & "\Crasher")
+
+        If folderExists = True Then
+
+            
+            reloadserverlist()
+            If count.Count = 0 Then
+            Else
+                ComboBox1.SelectedIndex = 0
+            End If
+
+
+        Else
+            My.Computer.FileSystem.CreateDirectory(appdataPath & "\Crasher")
+        End If
+
+       
+
+
+
+
+    End Sub
+    Private Sub reloadserverlist()
+
+        ComboBox1.Items.Clear()
+        Dim files() As String = System.IO.Directory.GetFiles(appdataPath & "\Crasher")
+        Dim di As New IO.DirectoryInfo(appdataPath & "\Crasher")
+        Dim diar1 As IO.FileInfo() = di.GetFiles()
+        Dim dra As IO.FileInfo
+
+
+        For Each dra In diar1
+            'Dim strsplit() As String = Split(dra.FullName.ToString, ".")
+            ComboBox1.Items.Add(System.IO.Path.GetFileNameWithoutExtension(dra.FullName))
+
+        Next
+
+        count = My.Computer.FileSystem.GetFiles(appdataPath & "\Crasher")
+    End Sub
+
+
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+
+
+
+
+        Dim fileExists1 As Boolean
+        fileExists1 = My.Computer.FileSystem.FileExists(appdataPath & "\Crasher\" & ComboBox1.Text & ".Server")
+        If fileExists1 = True Then
+
+            Dim file4 As New FileStream(appdataPath & "\Crasher\" & ComboBox1.Text & ".Server", FileMode.Open, FileAccess.Read)
+
+            Dim br As New BinaryReader(file4)
+
+            ' Dim ip As String
+            'Dim port As String
+
+
+
+
+            TextBox2.Text = br.ReadString
+            TextBox3.Text = br.ReadString
+
+
+            br.Close()
+            file4.Close()
+
+
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        
+
+        If ComboBox1.Text = "" Or ComboBox1.Text = " " Then
+            NotifyIcon1.BalloonTipIcon = ToolTipIcon.Error
+            NotifyIcon1.BalloonTipTitle = "Server Name"
+            NotifyIcon1.BalloonTipText = "Enter a Server name!"
+            NotifyIcon1.ShowBalloonTip(7000)
+
+        Else
+
+            Dim file4 As New FileStream(appdataPath & "\Crasher\" & ComboBox1.Text & ".Server", FileMode.Create, FileAccess.Write)
+
+
+
+
+            Dim bw1 As New BinaryWriter(file4)
+
+            bw1.Write(TextBox2.Text)
+            bw1.Write(TextBox3.Text)
+
+            bw1.Close()
+            file4.Close()
+
+
+
+
+
+            NotifyIcon1.BalloonTipIcon = ToolTipIcon.Info
+            NotifyIcon1.BalloonTipTitle = "Saved"
+            NotifyIcon1.BalloonTipText = "Server Saved!"
+
+            NotifyIcon1.ShowBalloonTip(7000)
+
+            ' ComboBox1.Text = ""
+            reloadserverlist()
+            'Form1_Load(Nothing, Nothing)
+
+        End If
+
+       
+
+
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+
+        Dim SetupPath As String = Application.StartupPath & "\Disclaimer.pdf"
+
+
+        Dim mypro As New Process
+        'mypro.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        mypro.StartInfo.FileName = SetupPath
+        'mypro.StartInfo.Arguments = " " & TextBox2.Text & " " & TextBox3.Text
+        mypro.Start()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+
+
+        My.Computer.FileSystem.DeleteFile(appdataPath & "\Crasher\" & ComboBox1.Text & ".Server", FileIO.UIOption.OnlyErrorDialogs, FileIO.RecycleOption.DeletePermanently)
+
+        ComboBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+
+        NotifyIcon1.BalloonTipIcon = ToolTipIcon.Info
+        NotifyIcon1.BalloonTipTitle = "Deleted"
+        NotifyIcon1.BalloonTipText = "Server Deleted!"
+        NotifyIcon1.ShowBalloonTip(7000)
+
+        reloadserverlist()
+
+
+
+
+        'Form1_Load(Me, New System.EventArgs)
+        'Me.Show()
+    End Sub
+End Class
